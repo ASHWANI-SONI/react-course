a8985b12ec521ecb8b425182e7e134084158bc4d
ashwani072 <ashwanigeetanandsoni@gmail.com>
2023-06-27 17:11:56 +0530

    basics of React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Greeting() {
+  return <div>
+    <h3>hello world!!</h3>
+    <Person />
+    <Message />
+  </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+
+const Person = () => <h1>Person</h1>;
+
+const Message = () => {
+    return (
+        <h3>message from developer</h3>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Greeting />);
